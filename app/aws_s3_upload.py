dc82a20c96e8601b64aac2e083f3ceac8830ddab
Bill Adams <vikingbill@gmail.com>
2023-02-22 00:16:18 -0500

    remove greenlet

--- app/aws_s3_upload.py
+++ app/aws_s3_upload.py
@@ -12,7 +12,6 @@ s3 = boto3.client(
    "s3",
    aws_access_key_id=os.environ.get("S3_KEY"),
    aws_secret_access_key=os.environ.get("S3_SECRET"),
-   endpoint_url="https://fatalbanana.onrender.com/",
 )
 
 
